dc6a51d587b848665a3785ebd1f3817b077d39e5
Chris Padilla <cdpadilla42@gmail.com>
2020-01-06 10:14:15 -0600

    Create new task form

--- src/index.js
+++ src/index.js
@@ -5,7 +5,22 @@ newProjectTask('Chris', 'Me!', 1, 'Jan', 'To-Do');
 console.log(projectsContainer.projects);
 console.log(projectsContainer.showProject);
 
+const taskForm = document.querySelector('.task-form');
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('form submitted!');
+});
+
+const extractFormData = function (e) {
+  let title = document.querySelector("#title").value;
+  let description = document.querySelector('#description').value;
+  let priority = document.querySelector('#title').value;
+  let dueDate = document.querySelector('#due-date').value;
+  let project = document.querySelector('#project').value;
+  newProjectTask(title, description, priority, dueDate, project);
+  return;
+}
 
 // Next: Basic display. Show Projects and tasks.
-// add buttons with functionality
+// add buttons with functionality - EXTRACT RADIO BUTTON VALUE
 // Little bit at a time, friend
