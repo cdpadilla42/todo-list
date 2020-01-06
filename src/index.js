3620ef5a8075d9f63164f4cf297865e4f37457fb
Chris Padilla <cdpadilla42@gmail.com>
2020-01-06 14:11:24 -0600

    Add project display module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import newProjectTask from './model/newProjectTask';
 import projectsContainer from './model/projects';
+import displayProject from './views/accessProjects';
+
 
 newProjectTask('Chris', 'Me!', 1, 'Jan', 'To-Do');
-console.log(projectsContainer.projects);
-console.log(projectsContainer.showProject);
+displayProject.displayTasks('To-Do');
 
 const taskForm = document.querySelector('.task-form');
 taskForm.addEventListener('submit', (e) => {
@@ -12,14 +13,13 @@ taskForm.addEventListener('submit', (e) => {
 });
 
 const extractFormData = function (e) {
-  let title = document.querySelector("#title").value;
-  let description = document.querySelector('#description').value;
-  let priority = document.querySelector('#title').value;
-  let dueDate = document.querySelector('#due-date').value;
-  let project = document.querySelector('#project').value;
+  const title = document.querySelector("#title").value;
+  const description = document.querySelector('#description').value;
+  const priority = document.querySelector('#title').value;
+  const dueDate = document.querySelector('#due-date').value;
+  const project = document.querySelector('#project').value;
   newProjectTask(title, description, priority, dueDate, project);
-  return;
-}
+};
 
 // Next: Basic display. Show Projects and tasks.
 // add buttons with functionality - EXTRACT RADIO BUTTON VALUE
--- src/views/accessProjects.js
+++ src/views/accessProjects.js
@@ -1,1 +1,28 @@
-// pulls data from projects for DOM ellements
+// pulls data from projects for DOM elements
+import projectsContainer from '../model/projects';
+
+
+const displayProject = (function () {
+  const createTaskDisplay = function(task, projectDiv) {
+    const displayedTask = document.createElement('div');
+    // displayedTask.attributes.add('class', `${task.title}`);
+    displayedTask.innerText = `${task.title}, ${task.description}`;
+    projectDiv.appendChild(displayedTask);
+  };
+  
+  const displayTasks = function (project) {
+    const projectDiv = document.createElement('div');
+    const currentProject = projectsContainer.showProject(project);
+    
+    for(var i = 0; i < currentProject.length; i++) {
+      console.log(currentProject[i]);
+      createTaskDisplay(currentProject[i], projectDiv);
+    }
+    const content = document.querySelector('#content');
+    content.appendChild(projectDiv);
+    
+  };
+  return { displayTasks, createTaskDisplay };
+}());
+
+export { displayProject as default };
