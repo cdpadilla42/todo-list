00260efcca36d3ac12be26aada022e9d91973808
Chris Padilla <cdpadilla42@gmail.com>
2020-01-09 12:09:30 -0600

    Integrate form close with new task submission. Debug Task submission in modal div.

--- src/events/tasks.js
+++ src/events/tasks.js
@@ -1,27 +1,44 @@
 import newTaskForm from '../views/taskForm';
+import formEvents from './forms';
+import newProjectTask from '../model/newProjectTask';
+import displayProject from '../views/accessProjects';
 
 const taskEvents = (function() {
   const displayTaskForm = function() {
-
-    // HEYYYYYY!!! REFACTOR ME SO THAT I FIT IN A DIV NAMED "task-form" class SO I MAY CLOSE
     document.querySelector('.modal-content').appendChild(newTaskForm);
-    console.log("hey");    
-
-    // const taskForm = document.querySelector('.task-form'); 
-    // taskForm.appendChild(newTaskForm);
-    // document.querySelector('#modal-content').appendChild(taskForm);
-    // taskForm.addEventListener('submit', (e) => {
-    //   e.preventDefault();
-    //   alert('form submitted!');
-    //   extractTaskFormData();
-    // });
-    
+    const taskForm = document.querySelector('.task-form');
+    taskForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      extractTaskFormData();
+      // form events - remove modal content
+      formEvents.closeForm();
+    });
   }
 
   const closeTaskForm = function() {
     let taskForm = document.querySelector('.project-form');
     document.querySelector('#content').removeChild(taskForm);
   }
+
+   const submitNewTaskForm = function() {
+    //submit data from form, like below
+
+    //  const title = document.querySelector('#project-title').value;
+    //  projectsContainer.addProjectToContainer(title);
+    //  displayProjectNav.refreshProjectDisplay();
+    //  formEvents.closeForm();
+   };
+
+   const extractTaskFormData = function() {
+     const title = document.querySelector('#title').value;
+     const description = document.querySelector('#description').value;
+     const priority = document.querySelector('#priority').value;
+     const dueDate = document.querySelector('#due-date').value;
+     const project = document.querySelector('#project').value;
+     newProjectTask(title, description, priority, dueDate, project);
+
+     displayProject.refreshTaskDisplay();
+   };
 
   return { displayTaskForm, closeTaskForm };
 }());
